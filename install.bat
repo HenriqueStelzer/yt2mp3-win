05d3c5ad5b58bad7d2f95d5da62decdd9860519b
Henrique Stelzer de Oliveira <henriquestelzerdeoliveira@gmail.com>
2025-04-22 18:39:36 -0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,23 @@
+@echo off
+set "APP_DIR=%USERPROFILE%\.local\bin\yt2mp3"
+
+if "%1"=="install" (
+    if not exist "%APP_DIR%" mkdir "%APP_DIR%"
+    xcopy /E /I /Y "..\yt2mp3" "%APP_DIR%"
+    pip install -r "%APP_DIR%\requirements.txt"
+    winget install ffmpeg -e --id Gyan.FFmpeg
+    echo doskey yt2mp3=python "%APP_DIR%\app.py" $* > "%USERPROFILE%\yt2mp3_alias.bat"
+    echo Installation completed.
+    exit /b
+)
+
+if "%1"=="uninstall" (
+    rmdir /S /Q "%APP_DIR%"
+    del "%USERPROFILE%\yt2mp3_alias.bat"
+    echo Uninstallation completed.
+    exit /b
+)
+
+echo Usage:
+echo   install.bat install   - to install yt2mp3
+echo   install.bat uninstall - to uninstall yt2mp3
